39570ed05b0f142de1620dc42304cac4f8b6964a
Timothy Shores <timothymshores@gmail.com>
2019-01-24 13:44:51 -0600

    Created Person class and instantiated instantiated one test object

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,9 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(props) {
+        this.name = props.name;
+        thisage = props.age;
+        thislocation = props.location;
+        thisgender = props.gender;
+    }
+}
